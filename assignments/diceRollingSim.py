e7b17e81ffb6081bf8c90606ab56658759ce5cad
Kieran Thakkar <kieranthakkar@gmail.com>
2023-09-13 21:13:34 +0100

    Comments added

--- assignments/diceRollingSim.py
+++ assignments/diceRollingSim.py
@@ -1,3 +1,7 @@
+# DICE ROLLER w/ upper and lower bounds as arguements in the function declaration.
+# By default, dice are 6-sided with min. roll = 1 and max. roll = 6.
+# Program will ask if user wants to roll again, inputting "no" ends the program.
+
 # Importing the 'random' module to generate random integers
 import random
 
@@ -10,10 +14,13 @@ def dice_roller(min: int = 1, max: int = 6) -> str:
         # If minimum > maximum, values are swapped to prevent an error 
         # random.randint(a,b) requires b>=a.
         min,max = max,min
-    
+
+    # Generating and printing the result    
     result = random.randint(min,max)
     print(f"[{min}-{max}] You rolled a {result}!")
 
+    # User input and re-roll loop
+    # Program requires "no" or "exit" input to break
     while True:
         reroll = input("Would you like to roll again? ")
         if reroll.lower() in "yes":
