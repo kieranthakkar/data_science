a18d2c8d4031d8d4c3ad9bf0c998e94b67c86bdd
Kieran Thakkar <kieranthakkar@gmail.com>
2023-09-14 15:17:36 +0100

    Upper-bound input and validation

--- assignments/guessTheNumber.py
+++ assignments/guessTheNumber.py
@@ -7,4 +7,22 @@
 import random
 
 def number_guesser():
-    pass
+    # Defining function / local variables.
+    # Take user input to set the upper-bound of range - program requires a positive integer input.
+    upperBound = input("Type a number (upper bound): ")
+    # Checking to see if user input is a positive integer.
+    if upperBound.isdigit():
+        # If input is +integer, cast upperBound to type int.
+        upperBound = int(upperBound)
+        # Check to ensure there is a non-zero range. If input == 0, function/program is restarted.
+        if upperBound == 0:
+            print("Range can't be equal to 0. Please enter a different number.")
+            return number_guesser()
+        # Numbers reaching here have passed the checks and are then used to generate a random integer.
+        else:
+            ans = random.randint(0, upperBound)
+    # Restart program if input != positive integer
+    else:
+        print("Please type a positive integer.")
+        return number_guesser()
+    
