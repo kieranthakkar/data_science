cfeb7ea2ec8f9c7b9338f1560f036bf243dc299f
Kieran Thakkar <kieranthakkar@gmail.com>
2023-09-14 14:07:37 +0100

    Guess the number added. Function declared and random imported

---
+++ assignments/guessTheNumber.py
@@ -0,0 +1,10 @@
+# kieranthakkar, Murtaza3D
+# DICE ROLLER w/ upper and lower bounds as arguements in the function declaration.
+# By default, dice are 6-sided with min. roll = 1 and max. roll = 6.
+# Program will ask if user wants to roll again, inputting "no" ends the program.
+
+# Importing the 'random' module to generate random integers
+import random
+
+def number_guesser():
+    pass
