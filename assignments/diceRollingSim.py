1497f4ffd5fd9e0e5cd78ab9b5155990a01773ba
Kieran Thakkar <kieranthakkar@gmail.com>
2023-09-15 12:20:55 +0100

    Added Tidiane's name

--- assignments/diceRollingSim.py
+++ assignments/diceRollingSim.py
@@ -1,4 +1,4 @@
-# kieranthakkar, Murtaza3D
+# kieranthakkar, Murtaza3D, Tidiane
 # DICE ROLLER w/ upper and lower bounds as arguements in the function declaration.
 # By default, dice are 6-sided with min. roll = 1 and max. roll = 6.
 # Program will ask if user wants to roll again, inputting "no" ends the program.
