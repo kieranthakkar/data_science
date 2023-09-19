a599a25b31c0c7c816c88b3f3abb883fde187da0
Kieran Thakkar <kieranthakkar@gmail.com>
2023-09-19 13:45:27 +0100

    Description changed

--- assignments/adventureGame.py
+++ assignments/adventureGame.py
@@ -1,7 +1,7 @@
 # kieranthakkar, tanu
 # ADVENTURE GAME - a complete time-trials text game
-# the program will let users move through rooms based on user input and get descriptions of each room. To create this, you’ll need to establish the direction in which the user can move, a way to track how far the user has moved (and therefore which room he/she is in), and to print out a description.
-# You’ll also need to set limits for how far the user can move. In other words, create “walls” around the rooms that tell the user, “You can’t move further in this direction.”
+# Users are timed as they move through rooms w/ input.
+# A map is displayed with live tracking and descriptions of each room.
 
 
 # 'os' module for 'cls' command to clear the terminal
